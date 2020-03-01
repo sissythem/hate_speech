8a0f5eb1d65c7d524ffed5c750ff4832ba236855
Sissy Themeli <skthemeli@gmail.com>
2020-03-01 13:03:01 +0200

    add mysql docker

--- docs/source/index.rst
+++ docs/source/index.rst
@@ -7,7 +7,7 @@ Welcome to Hate-Speech-Detection's documentation!
 =================================================
 
 .. toctree::
-   :maxdepth: 2
+   :maxdepth: 4
    :caption: Contents:
 
 
---
+++ scripts/entrypoint.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+port=8000
+exec_path=/hate_speech
+
+# Registry db initializations
+
+until python $exec_path/manage.py makemigrations > /dev/null 2>&1 ;do
+      echo "Waiting mysql docker to setup........"
+      sleep 1
+done
+
+echo "Initializing....."
+python $exec_path/manage.py makemigrations
+python $exec_path/manage.py migrate
+python $exec_path/manage.py migrate --run-syncdb
+
+# Create superuser from localsettings
+username="root"
+password="root"
+
+email=$username"@example.com"
+
+echo "Creating super user....."
+
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('$username', '$email', '$password')" | python $exec_path/manage.py shell
+
+echo "Starting web server...."
+
+# python $exec_path/manage.py runserver 0.0.0.0:$port
+exec gunicorn  -w 9 -b 0.0.0.0:$port hate_speech.wsgi \
+		--log-level debug \
+                --backlog 0 \
+                --timeout 120
